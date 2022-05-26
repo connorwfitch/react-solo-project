8482b269a78a2ec655e4f87e032fa1901c3595af
connorwfitch <connorwfitch@gmail.com>
2022-05-26 16:36:20 -0400

    validate login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,19 +1,32 @@
 // External modules
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 
 // Internal modules
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 /* 
 -------------------ROUTES-------------------
 */
 // POST /api/session (login)
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.login({ credential, password });
