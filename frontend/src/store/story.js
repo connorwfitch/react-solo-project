f1ec2affd4ba85936824c2cd4d20eb84b7074d47
connorwfitch <connorwfitch@gmail.com>
2022-05-31 14:10:55 -0400

    thunk for getting stories is working

--- frontend/src/store/story.js
+++ frontend/src/store/story.js
@@ -14,8 +14,8 @@ export const getStories = () => async dispatch => {
   const response = await csrfFetch('/api/stories');
 
   if (response.ok) {
-    const stories = await response.json();
-    dispatch(loadAll(stories));
+    const output = await response.json();
+    dispatch(loadAll(output.stories));
   }
 }
 
@@ -34,6 +34,7 @@ const storiesReducer = (state = initialState, action) => {
   let newState = {};
   switch (action.type) {
     case LOAD_ALL:
+      console.log('--------------TESTING--------------', typeof action.stories);
       action.stories.forEach(story => {
         newState[story.id] = story;
       });
