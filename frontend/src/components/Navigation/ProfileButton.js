f705a290a70393cc7f5e3e87bd298fcff9d4c3f9
connorwfitch <connorwfitch@gmail.com>
2022-05-31 15:46:21 -0400

    frustrating bug with the dropdown menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,20 +1,29 @@
+// External modules
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
+
+// Internal modules
 import * as sessionActions from '../../store/session';
+import './ProfileButton.css';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
 
   const openMenu = () => {
+    console.log('---------HERE----------', showMenu)
     if (showMenu) return;
     setShowMenu(true);
   };
 
   useEffect(() => {
+
+    console.log('---------HERE2----------', showMenu)
+
     if (!showMenu) return;
 
     const closeMenu = () => {
+      console.log('---------HERE3----------', showMenu)
       setShowMenu(false);
     };
 
@@ -30,7 +39,8 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button onClick={openMenu} className='button white'>
+        {user.username}
         <i className="fa-solid fa-user"></i>
       </button>
       {showMenu && (
