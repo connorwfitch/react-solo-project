240acf448205fadee13f85d4dbd536806315840f
connorwfitch <connorwfitch@gmail.com>
2022-07-26 17:39:53 -0400

    removed topic database stuff

--- backend/db/seeders/20220526180123-demo-user.js
+++ backend/db/seeders/20220526180123-demo-user.js
@@ -6,7 +6,7 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [
       {
         email: 'demo@user.io',
-        username: 'Demo-lition',
+        username: 'Demo',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
