25fedc9c2d18e7840b618899f5ba5e882a7a861e
connorwfitch <connorwfitch@gmail.com>
2022-08-01 13:16:30 -0400

    comments seeder

--- backend/db/seeders/20220527232819-CommentSeeder.js
+++ backend/db/seeders/20220527232819-CommentSeeder.js
@@ -4,9 +4,39 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Comments', [
       {
-        content: 'Wow, I did not even think of this until now!',
+        content: 'Thank you, can\'t wait to try these out!',
         userId: 2,
         storyId: 1
+      },
+      {
+        content: 'YUMMY!',
+        userId: 3,
+        storyId: 2
+      },
+      {
+        content: 'Oh, so this is why Dutch kids are the happiest in the world!',
+        userId: 1,
+        storyId: 2
+      },
+      {
+        content: 'Very interesting! I never knew about the origins of the name!',
+        userId: 1,
+        storyId: 3
+      },
+      {
+        content: 'Can I hire the Dutch transit workers for my startup\'s UX team?',
+        userId: 3,
+        storyId: 4
+      },
+      {
+        content: 'Putting my life savings into tulip stock options!',
+        userId: 2,
+        storyId: 5
+      },
+      {
+        content: 'Three cheers for LGBTQ+ rights! Hip hip! Hooray!',
+        userId: 3,
+        storyId: 6
       },
     ], {});
   },
